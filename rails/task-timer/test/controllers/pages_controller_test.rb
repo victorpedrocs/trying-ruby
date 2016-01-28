d64eadb2f1244cc0b17274eebc498a8bdc66d1f5
vpcsilva <vpcx5@yahoo.com.br>
2016-01-27 23:05:24 -0200

    Finishing the third part of the tutorial * Using the assts/images to have images in the application * Modifying the default css for the application * Some updates in the tests * Major change in the views

--- rails/task-timer/test/controllers/pages_controller_test.rb
+++ rails/task-timer/test/controllers/pages_controller_test.rb
@@ -4,18 +4,32 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    # assert_select "title", "Welcome"
+
+  end
+
+  test "should get the right title in home page" do
+    get :home
+    assert_select "title", "Home"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    # assert_select "title", "About"
+  end
+
+  test "should get the right title in about page" do
+    get :about
+    assert_select "title", "About"
   end
 
   test "should get directions" do
     get :directions
     assert_response :success
+  end
+
+  test "should get the right title in directions page" do
+    get :directions
+    assert_select "title", "Directions"
   end
 
 end
