bcc0103d08f5eb6e65120ca774075673d17fdf76
vpcsilva <vpcx5@yahoo.com.br>
2016-03-07 17:55:09 -0300

    Adding some scopes in timer class for querying

--- rails/task-timer/app/models/timer.rb
+++ rails/task-timer/app/models/timer.rb
@@ -1,3 +1,8 @@
 class Timer < ActiveRecord::Base
+  scope :recent, ->(num=10) { order(created_at: :desc).limit num }
+  scope :by_task, ->(task) { where(task_id: task) }
+  scope :open, -> { where(finish: nil) }
+  scope :closed, -> { where.not(start: nil, finish: nil) }
+
   belongs_to :task
 end
