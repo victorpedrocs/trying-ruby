89077c6d5072013901a151e2ab5ac6803a05d127
vpcsilva <vpcx5@yahoo.com.br>
2016-03-07 23:36:25 -0300

    Showing the tasks and hours in the show page action. Adding a link to the project in the task show action

--- rails/task-timer/app/controllers/projects_controller.rb
+++ rails/task-timer/app/controllers/projects_controller.rb
@@ -65,6 +65,7 @@ class ProjectsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
+      @tasks = @project.tasks.includes(:timers)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
